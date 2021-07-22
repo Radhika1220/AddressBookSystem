cb09f9bd0c6a7bc3d2146552ddb50f363b9d6c3a
Radhika <radhika.shankar1220@gmail.com>
2021-07-22 12:03:20 +0530

    [Radhika] avoid duplicate entries

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -7,7 +7,8 @@ namespace AddressBookSystem
 
     {
      
-        public static Dictionary<string, LinkedList<AddrBook.Person>> addressBook = new Dictionary<string, LinkedList<AddrBook.Person>>();
+        public static Dictionary<string, List<AddrBook>> addressBook = new Dictionary<string, List<AddrBook>>();
+
         static void Main(string[] args)
         {
 
@@ -26,15 +27,38 @@ namespace AddressBookSystem
                 while (noOfContact > 0)
                 {
                     Console.WriteLine("Enter the details of contact to be added: ");
-                    addrBook.GetCustomer();
+
+                    Console.Write("Enter First Name: ");
+                    string FirstName = Console.ReadLine();
+
+                    Console.Write("Enter Last Name: ");
+                    string LastName = Console.ReadLine();
+
+                    Console.Write("Enter Phone Number: ");
+                    string PhoneNumber = Console.ReadLine();
+
+                    Console.Write("Enter Address : ");
+                    string Addresses = Console.ReadLine();
+
+                    Console.Write("Enter City : ");
+                    string City = Console.ReadLine();
+
+                    Console.Write("Enter State : ");
+                    string State = Console.ReadLine();
+
+                    Console.Write("Enter ZipCode: ");
+                    string ZipCode = Console.ReadLine();
+
+                    Console.Write("Enter EmailId: ");
+                    string EmailId = Console.ReadLine();
+                    addrBook.GetCustomer(FirstName, LastName, PhoneNumber, Addresses, City, State, ZipCode, EmailId);
                     noOfContact--;
                     Console.WriteLine(" ");
                     addrBook.ListingPeople();
                 }
                 Console.WriteLine("1.To modify the details");
-                    Console.WriteLine("2.To remove the details");
+                Console.WriteLine("2.To remove the details");
                 Console.WriteLine("3.Exit!");
-                //   int check = Convert.ToInt32(Console.ReadLine());
                 switch (Console.ReadLine())
                 {
                     case "1":
@@ -67,7 +91,7 @@ namespace AddressBookSystem
                 noOfBooks++;
             }
 
-            foreach (KeyValuePair<string, LinkedList<AddrBook.Person>> addr in addressBook)
+            foreach(KeyValuePair<string, List<AddrBook>> addr in addressBook)
             {
                 Console.WriteLine("The address Books are:{0}", addr.Key);
 
